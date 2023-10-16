6d9d6fd80ddcacc563f100714562a3c5cbb10331
luiscarlos <luiscarlos8pe@gmail.com>
2023-10-16 14:26:57 -0300

    Ḿapeando elementos

--- src/main/java/br/com/carlos/page/LoginPO.java
+++ src/main/java/br/com/carlos/page/LoginPO.java
@@ -1,10 +1,30 @@
 package br.com.carlos.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPO extends BasePO {
 
-    protected LoginPO(WebDriver driver) {
+    /**
+     * Construtor padrao para uma nova instancia da pagina de login
+     * 
+     * @param driver driver da pagina de login
+     */
+
+    @FindBy(id = "email")
+    public WebElement inputEmail;
+
+    @FindBy(id = "senha")
+    public WebElement inputSenha;
+
+    @FindBy(id = "btn-entrar")
+    public WebElement btnEntrar;
+
+    @FindBy(css = "form.form-login>div.alert>span")
+    public WebElement spanMensagem;
+
+    public LoginPO(WebDriver driver) {
         super(driver);
         // TODO Auto-generated constructor stub
     }
